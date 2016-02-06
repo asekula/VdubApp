b64673302f5f911ab642e1e734890b7205312ae1
Alex <alex@alexs-macbook-air.local>
2016-02-06 12:50:55 -0500

    testing

--- VdubApp/ViewController.swift
+++ VdubApp/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Testing.")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
