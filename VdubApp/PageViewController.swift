a4da948feead25c355f8a14c48ff8da4832f3e3a
Alex <alex@alexs-macbook-air.local>
2016-02-06 13:31:37 -0500

    pageviewcontroller added

---
+++ VdubApp/PageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PageViewController.swift
+//  VdubApp
+//
+//  Created by Alex on 2/6/16.
+//  Copyright © 2016 Alex. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("test")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- VdubApp/ViewController.swift
+++ VdubApp/ViewController.swift
@@ -16,11 +16,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func SwipeRight(sender: UIScreenEdgePanGestureRecognizer) {
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
