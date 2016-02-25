2c4bd189c78ed84a9ae8eef3182907c0dd2b8e09
Alex <alex@alexs-macbook-air.local>
2016-02-24 22:44:43 -0500

    fixed special week cases (brunch/weekend at vdub)

--- VdubApp/MenuRetriever.swift
+++ VdubApp/MenuRetriever.swift
@@ -18,15 +18,18 @@ class MenuRetriever {
     // First check if it's stored in a local file.
     // If not, get from api, then store in file.
     func get(hall: Int, offset: Int) -> [[String:[String]]] {
-         
+        
         let day = Date.getDayNumber(offset)
         let month = Date.getMonthNumber(offset)
  
-        
         var url = "&day=\(day)&month=\(month)"
         
         switch hall {
-        case 0: url = "vdub"+url
+        case 0:
+            url = "vdub"+url
+            if Date.isWeekend(offset) {
+                return [[String:[String]](),[String:[String]](),[String:[String]]()]
+            }
         case 1: url = "ratty"+url
         case 2: return andrews(offset)
         default: return [[:]]
@@ -47,6 +50,9 @@ class MenuRetriever {
             var actualmeal = meal
             if hall == 0 && meal > 0 {
                 actualmeal += 1
+            }
+            if hall == 1 && Date.getWeekday(offset) == "Sunday" && meal > 0{
+                actualmeal = meal - 1
             }
             
             let dict = data[actualmeal].dictionaryValue
